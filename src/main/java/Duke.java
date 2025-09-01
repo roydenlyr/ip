68a545112778fcfd87efee4a7a1057029d2aa5a8
Royden <roydenlyr@gmail.com>
2025-09-02 02:22:08 +0800

    feat(task status): add status for each task
    - Implement Task class to store task detail
    - Implement mark and unmark command
    - Implement extractTrailingNumber function to update desired task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,29 +3,47 @@ import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
-        int taskCounter = 0;
+        Task[] tasks = new Task[100];
         printMessage("Hello! I'm Duke.\n" +
                 "What can I do for you?");
 
         while(true) {
             String line = new Scanner(System.in).nextLine();
             String sanitizedTask = sanitizeMessage(line);
+            String message = "";
+            int taskIndex = 0;
             switch(sanitizedTask) {
             case "bye":
                 printMessage("Bye. Hope to see you again soon!");
                 return;
             case "list":
-                String list = "";
-                for (int i = 0; i < taskCounter; i++) {
-                    list += (i + 1) + ": " + tasks[i] + (i == taskCounter - 1 ? "" : "\n");
+                message = "Here are the tasks in your list: \n";
+                for(int i = 0; i < Task.getNumOfTasks(); i++) {
+                    message += (i + 1) + "." + tasks[i].getStatusIcon() + " " +
+                            tasks[i].getDescription() +
+                            (i == Task.getNumOfTasks() - 1 ? "" : "\n");
                 }
-                printMessage(list);
+                printMessage(message);
+                break;
+            case "mark":
+                taskIndex = extractTrailingNumber(line) - 1;
+                tasks[taskIndex].setDone(true);
+                message = "Nice! I've marked this task as done:\n" +
+                        tasks[taskIndex].getStatusIcon() + " " +
+                        tasks[taskIndex].getDescription();
+                printMessage(message);
+                break;
+            case "unmark":
+                taskIndex = extractTrailingNumber(line) - 1;
+                tasks[taskIndex].setDone(false);
+                message = "Ok, I've marked this task as not done yet:\n" +
+                        tasks[taskIndex].getStatusIcon() + " " +
+                        tasks[taskIndex].getDescription();
+                printMessage(message);
                 break;
             default:
-                tasks[taskCounter] = line;
-                printMessage("added: " + tasks[taskCounter]);
-                taskCounter++;
+                tasks[Task.getNumOfTasks()] = new Task(line);
+                printMessage("added: " + tasks[Task.getNumOfTasks() - 1].getDescription());
                 break;
             }
         }
@@ -38,6 +56,21 @@ public class Duke {
     }
 
     public static String sanitizeMessage(String message) {
-        return message.trim().toLowerCase();
+        message = message.trim().toLowerCase();
+        if (message.contains("unmark")) {
+            return "unmark";
+        }
+        if (message.contains("mark")) {
+            return "mark";
+        }
+        return message;
+    }
+
+    public static int extractTrailingNumber(String message) {
+        int lastIndex = message.length() - 1;
+        while (lastIndex >= 0 && Character.isDigit(message.charAt(lastIndex))) {
+            lastIndex--;
+        }
+        return Integer.parseInt(message.substring(lastIndex + 1));
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    private static int numOfTasks = 0;
+
+    public Task(String description) {
+        setDescription(description);
+        setDone(false);
+        numOfTasks++;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public static int getNumOfTasks() {
+        return numOfTasks;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+}
