69e5d086c68e5043781d8cf7fc04f0b81f5c8f33
Royden <roydenlyr@gmail.com>
2025-09-01 14:44:31 +0800

    Add echo feature, exit command, and style output message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\n" +
-                "What can I do for you?\n\n" +
-                "Bye. Hope to see you again soon!");
+        printMessage("Hello! I'm Duke\n" +
+                "What can I do for you?");
+
+        while(true) {
+            String line = new Scanner(System.in).nextLine();
+            if(sanitizeMessage(line).equals("bye")){
+                printMessage("Bye. Hope to see you again soon!");
+                return;
+            }
+            printMessage(line);
+        }
+    }
+
+    public static void printMessage (String message) {
+        System.out.println("\n====================");
+        System.out.println(message);
+        System.out.println("====================\n");
+    }
+
+    public static String sanitizeMessage(String message) {
+        return message.trim().toLowerCase();
     }
 }
